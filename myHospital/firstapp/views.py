a797818fd250b386acb058bc52b74a8a00587681
Ghalib92 <ghalibmohammad92@gmail.com>
2025-01-15 08:45:22 +0300

    changes

--- myHospital/firstapp/views.py
+++ myHospital/firstapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
+from .forms import PhysicalAppointmentForm
 
 # Create your views here.
 
@@ -11,8 +12,16 @@ def online(request):
     template = loader.get_template('online_consultations.html')
     return HttpResponse(template.render())  
 def physical(request):
-    template = loader.get_template('physical_appointment.html')
-    return HttpResponse(template.render())  
+    if request.method == 'POST':
+        form = PhysicalAppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('physical_appointment')  # Replace with your success page
+    else:
+        form = PhysicalAppointmentForm()
+
+    return render(request, 'physical_appointment.html', {'form': form})
+
 def emergency(request):
     template = loader.get_template('emergency_care.html')
     return HttpResponse(template.render())  
