98446fff0981de1d1a9134c60b7d6c10b00a4308
Ghalib92 <ghalibmohammad92@gmail.com>
2025-01-15 11:34:40 +0300

    Build stage changes

--- myHospital/firstapp/admin.py
+++ myHospital/firstapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import PhysicalAppointment
 
 # Register your models here.
+# admin.py
+ 
+
+@admin.register(PhysicalAppointment)
+class PhysicalAppointmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_no', 'service_needed', 'appointment_date', 'created_at')
+    search_fields = ('name', 'email', 'phone_no', 'service_needed')
+    list_filter = ('service_needed', 'appointment_date')
+    ordering = ('-created_at',)  # Order by most recent creation date
--- myHospital/myHospital/settings.py
+++ myHospital/myHospital/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
+    
 ]
+JAZZMIN_SETTINGS = {
+    "site_title": "My Custom Admin",
+    "site_header": "Welcome to My Admin Panel",
+    "welcome_sign": "Manage Your System",
+    "show_sidebar": True,
+    "navigation_expanded": True,
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,7 +136,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
